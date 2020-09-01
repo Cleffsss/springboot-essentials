b92cc00995866293491c3ffa6b56c4e4d328b9bf
Joao Paulo Figueira <jp_figueira@outlook.com>
2020-09-01 11:27:52 -0400

    springboot test without a database

--- src/main/java/br/com/devdojo/model/Student.java
+++ src/main/java/br/com/devdojo/model/Student.java
@@ -1,40 +1,11 @@
 package br.com.devdojo.model;
 
-import java.util.ArrayList;
-import java.util.List;
 
-import static java.util.Arrays.asList;
+import javax.persistence.Entity;
 
-
-
-public class Student {
-    private int id;
+@Entity
+public class Student extends AbstractEntity {
     private String name;
-    public static List<Student> studentList;
-
-    static {
-        studentRepository();
-    }
-
-    public Student(int id, String name) {
-        this(name);
-        this.id = id;
-    }
-
-    public Student(int id) {
-        this.id = id;
-    }
-
-    public Student(String name) {
-        this.name = name;
-    }
-
-    public Student() {
-    }
-
-    private static void studentRepository(){
-        studentList = new ArrayList<>(asList(new Student(0,  "Deku"), new Student(1,"Todoroki")));
-    }
 
     public String getName() {
         return name;
@@ -42,28 +13,5 @@ public class Student {
 
     public void setName(String name) {
         this.name = name;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-
-        Student student = (Student) o;
-
-        return id == student.id;
-    }
-
-    @Override
-    public int hashCode() {
-        return id;
     }
 }
