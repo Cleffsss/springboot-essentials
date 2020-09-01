d8e49916978a26d0ab1ac5b66df159a2537cdb53
Joao Paulo Figueira <jp_figueira@outlook.com>
2020-09-01 11:30:26 -0400

    springboot test with database

--- src/main/java/br/com/devdojo/SpringBootEssentialsApplication.java
+++ src/main/java/br/com/devdojo/SpringBootEssentialsApplication.java
@@ -3,7 +3,7 @@ package br.com.devdojo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+// uhum
 
 @SpringBootApplication
 public class SpringBootEssentialsApplication {
